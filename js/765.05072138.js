4bb2bbb8274051c69f8a1d10738d0d428a437ecf
Kholod Ihor <kholodihor@gmail.com>
2022-04-26 19:23:28 +0300

    Updates

--- js/765.05072138.js
+++ js/765.05072138.js
@@ -1,2 +1,2 @@
 "use strict";(self["webpackChunk_1plus1"]=self["webpackChunk_1plus1"]||[]).push([[765],{4765:function(e,n,t){t.r(n),t.d(n,{default:function(){return p}});var r=t(3396);const s={class:"container"};function u(e,n,t,u,c,o){const i=(0,r.up)("CountryVoice"),a=(0,r.up)("CelebrityNews");return(0,r.wg)(),(0,r.iD)("div",s,[(0,r.Wm)(i),(0,r.Wm)(a)])}var c=t(830),o=t(3188),i={name:"MoviesView",components:{CountryVoice:c.Z,CelebrityNews:o.Z}},a=t(89);const l=(0,a.Z)(i,[["render",u]]);var p=l}}]);
-//# sourceMappingURL=765.6d2b0e07.js.map
+//# sourceMappingURL=765.05072138.js.map
